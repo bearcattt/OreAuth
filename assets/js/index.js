e64301f973508e157cba1b252d0f7f82fb31bb42
bearcattt <ewigglet8@gmail.com>
2024-12-10 14:25:47 -0500

    add button functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,17 @@
-  // optlib config
-    const auth = window.otplib.authenticator;
-    const secret = document.getElementById('secret-input');
+ const auth = window.otplib.authenticator;
+    const add = document.getElementById("add");
     auth.options = { step: 30 };
+
+ add.addEventListener("click", function() {
+  const secret = prompt("Please enter your verification code here.")
+  const token = authenticator.generate(token);
+   const validate = authenticator.verify({ token, secret });
+   if (validate) {
+     
+   } else {
+     alert("invalid verification code")
+   }
+}); 
+     
 
  
