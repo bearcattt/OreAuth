b9da473f6fc40763c42dd520f346036208a9f9cb
bearcattt <ewigglet8@gmail.com>
2024-12-10 14:46:28 -0500

    save and load from local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,18 @@
  const auth = window.otplib.authenticator;
-    const add = document.getElementById("add");
-    auth.options = { step: 30 };
+const add = document.getElementById("add");
+auth.options = { step: 30 };
 
- add.addEventListener("click", function() {
-  const secret = prompt("Please enter your verification code here.")
-  const token = auth.generate(secret);
+add.addEventListener("click", function () {
+    const secret = prompt("Please enter your 2FA code here.");
+    if (secret) {
+        let secrets = JSON.parse(localStorage.getItem("2FA-Token")) || [];
+        const token = auth.generate(secret);
+        secrets.push({ secret, token });
 
-  localStorage.setItem("secret", secret)
-}); 
-     
-
- 
+        
+        localStorage.setItem("2FA-Token", JSON.stringify(secrets));
+        console.log("2Fa code was added successfully!");
+    } else {
+        console.log("No 2FA code entered.");
+    }
+});
