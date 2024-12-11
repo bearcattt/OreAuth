fa6e62dc7a48c5ef2726e057d779cb9e9ef1da86
bearcattt <ewigglet8@gmail.com>
2024-12-10 19:04:59 -0500

    fixing

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,10 +8,9 @@ add.addEventListener("click", function () {
         let secrets = JSON.parse(localStorage.getItem("2FA-Token")) || [];
         const token = auth.generate(secret);
         secrets.push({ secret, token });
-
-        
         localStorage.setItem("2FA-Token", JSON.stringify(secrets));
         console.log("2FA code was added successfully!");
+     
     } else {
         console.log("No 2FA code entered.");
     }
